d64b5bd451a538dc2b5fc1422cd5723bf393e265
sakharkarZuhair <sakharkarzuhair210@gmail.com>
2022-04-03 00:31:36 -0400

    My Commit

---
+++ backend/models/productModel.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+});
+
+const movieSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    reviews: [reviewSchema],
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: { type: Number, required: true, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const Movie = mongoose.model("Movie", productSchema);
+
+export default Movie;
